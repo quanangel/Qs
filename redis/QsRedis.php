5dc9af8ffb37072b2c2cdc0d434d7c3f9c2c2ac9
Quan <quanangel@outlook.com>
2019-04-18 18:24:03 +0800

    调整--整体功能

--- redis/QsRedis.php
+++ redis/QsRedis.php
@@ -9,7 +9,6 @@
 namespace Qs\redis;
 
 Class QsRedis {
-    protected $tag; // 缓存名的标签
     protected $handler = null;
     protected $_config = array(
         'HOST'       => '127.0.0.1',    // 地址
@@ -33,153 +32,111 @@ Class QsRedis {
         if ( !empty( $this->_config['SELECT'] ) ) $this->handler->select($this->_config['SELECT']);
     }
 
-    /*
-     * Auth:Qs
-     * Name:判断是否存在该缓存名
-     * Note:
-     * @param   string  $name   需查找的缓存名
-     * @return  string
-     * Time:2017/10/18 17:07
-     */
-    public function has($name) {
-        $name = $this->_config['PREFIX_STATUS'] ? $this->getRealKey($name) : $name;
-        return $this->handler->get($name) ? true : false;
+    /**
+     * @Author : Qs
+     * @Name   : 判断是否存在该字符串类型的键
+     * @Note   : 
+     * @Time   : 2019/4/18 17:53
+     * @param   string        $name     需获取的键名
+     * @param   boolean|null  $prefix   是否需要前缀
+     **/
+    public function has($name, $prefix = null) {
+        $name = $this->getRealKey($name, $prefix);
+        return $this->handler->exists($name) ? true : false;
     }
 
-    /*
-     * Auth:Qs
-     * Name:获取缓存的值
-     * Note:
-     * @param   string      $name       需获取的缓存名
-     * @param   boolean     $default    默认返回值为false
+    /**
+     * @Author : Qs
+     * @Name   : 获取字符串类型的键值
+     * @Note   : 
+     * @Time   : 2019/4/18 17:53
+     * @param   string        $name     需获取的键名
+     * @param   boolean|null  $prefix   是否需要前缀
      * @return  string|array
-     * Time:2017/10/18 17:36
-     */
-    public function get($name, $default = false) {
-        $name = $this->_config['PREFIX_STATUS'] ? $this->getRealKey($name) : $name;
+     **/
+    public function get($name, $prefix = null) {
+        $name = $this->getRealKey($name, $prefix);
         $value = $this->handler->get($name);
-        if( is_null($value) ) return $default;
+        if( is_null($value) ) return false;
         $jsonData = json_decode($value, true);
         // 判断$jsonData是否完全等于NULL，是：直接返回$value的值，否：返回JSON格式化的数组$jsonData
         return (null === $jsonData) ? $value : $jsonData;
     }
 
-    /*
-     * Auth:Qs
-     * Name:
-     * Note:
-     * @param   string                  $name   设置的缓存名
-     * @param   object|array|string     $value  缓存的值
-     * @param   integer                 $expire 有效时间（秒）
+    /**
+     * @Author : Qs
+     * @Name   : 设置字符串类型的键值
+     * @Note   : // TODO: 添加SET带有的EX、PX、NX、XX http://redisdoc.com/string/set.html
+     * @Time   : 2017/10/18 23:49
+     * @param   string                  $name    设置的键名
+     * @param   object|array|string     $value   设置的值
+     * @param   integer                 $expire  有效时间（秒）
+     * @param   boolean|null            $prefix  是否需要前缀
      * @return  boolean
-     * Time:2017/10/18 23:49
-     */
-    public function set($name, $value, $expire = null) {
+     **/
+    public function set($name, $value, $expire = null, $prefix = null) {
         $expire = is_null($expire) ? $this->_config['EXPIRE'] : $expire;
 
-        if ( $this->tag && !$this->has($name) ) {$first = true;}
-
-        $key = $this->_config['PREFIX_STATUS'] ? $this->getRealKey($name) : $name;
+        $key = $this->getRealKey($name);
         $value = ( is_object($value) || is_array($value) ) ? json_encode($value, true) : $value;
         if ( is_int($expire) && $expire ) {
             $result = $this->handler->setex($key, $expire, $value);
         } else {
             $result = $this->handler->set($key, $value);
         }
-        isset($first) && $this->setTagItem($key);
         return $result;
     }
 
-    /*
-     * Auth:Qs
-     * Name:删除缓存
-     * Note:
-     * @param   string  $name   需删除的缓存名
+    /**
+     * @Author : Qs
+     * @Name   : 删除键值
+     * @Note   : 
+     * @Time   : 2019/4/18 18:10
+     * @param   string  $name   需删除的键名
+     * @param   boolean|null  $prefix   是否需要前缀
      * @return  boolean
-     * Time:2017/10/18 23:57
-     */
-    public function rm($name) {
-        $name = $this->_config['PREFIX_STATUS'] ? $this->getRealKey($name) : $name;
+     **/
+    public function rm($name, $prefix = null) {
+        $name = $this->getRealKey($name);
         return $this->handler->del($name);
     }
 
-    /*
-     * Auth:Qs
-     * Name:清除缓存
-     * Note:
-     * @param   string  $tag    标签名
-     * Time:2017/10/19 0:56
-     */
-    public function clear($tag = null) {
-        if ( $tag ) {
-            $keys = $this->getTagItem($tag);
-            foreach ( $keys as $key ) {
-                $this->handler->delete($key);
-            }
-            $this->rm('tag_' . md5($tag));
-            return true;
-        }
+    /**
+     * @Author : Qs
+     * @Name   : 清除数据
+     * @Note   : 
+     * @Time   : 2019/4/18 18:18
+     * @param   string|integer|null  $select    库名:all时为全库清除
+     **/
+    public function clear($select = null) {
+        if ( !is_null($select) && $select == 'all') $this->handler->select($select);
+        // 全部清楚
+        if ( $select == 'all') return $this->handler->flushAll();
         return $this->handler->flushDB();
     }
 
-    /*
-     * Auth:Qs
-     * Name:获取实际缓存名
-     * Note:
-     * @param   string  $name   需查找的缓存名
+    /**
+     * @Author : Qs
+     * @Name   : 获取实际存入的键名
+     * @Note   : 
+     * @Time   : 2019/4/18 17:01
+     * @param   string        $name     需查找的缓存名
+     * @param   boolean|null  $prefix   是否需要前缀
      * @return  string
-     * Time:2017/10/18 17:04
-     */
-    public function getRealKey($name){
-        return $this->_config['PREFIX'] . $name;
+     **/
+    public function getRealKey($name, $prefix = null){
+        $prefix = is_null($prefix) ? $this->_config['PREFIX_STATUS'] : $prefix;
+        $name = $prefix ? $this->_config['PREFIX'] . $name : $name;
+        return $name;
     }
 
-    /*
-     * Auth:Qs
-     * Name:更新缓存标识
-     * Note:
-     * @param   string  $name   缓存标识
-     * Time:2017/10/19 0:07
-     */
-    protected function setTagItem($name) {
-        if ( $this->tag ) {
-            $key = 'tag_' . md5($this->tag);
-            if ( $this->has($key) ) {
-                $value = explode(',', $this->get($key));
-                $value[] = $name;
-                $value = implode(',', array_unique($value));
-            } else {
-                $value = $name;
-            }
-            $this->set($key, $value, 0);
-        }
-    }
-
-    /*
-     * Auth:Qs
-     * Name:获取标签包含的缓存标识
-     * Note:
-     * @param   string  $tag    缓存标签
-     * @return  array
-     * Time:2017/10/19 0:53
-     */
-    public function getTagItem($tag) {
-        $key = 'tag_' . md5($tag);
-        $value = $this->get($key);
-        if ( $value ) {
-            return array_filter(explode(',', $value));
-        } else {
-            return [];
-        }
-    }
-
-    /*
-     * Auth:Qs
-     * Name:返回句柄对象，可执行其它高级方法
-     * Note:
+    /**
+     * @Author : Qs
+     * @Name   : 返回句柄对象，可执行其它redis方法
+     * @Note   : 
+     * @Time   : 2017/10/19 0:57
      * @return  object
-     * Time:2017/10/19 0:57
-     */
+     **/
     public function handler(){
         return $this->handler;
     }
